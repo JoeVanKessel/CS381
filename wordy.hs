102b77a4fb8fb48b0ee94394b27b941dd59fc20e
JoeVanKessel <vankessj@oregonstate.edu>
2020-02-25 19:20:37 -0800

    Add files via upload

--- wordy.hs
+++ wordy.hs
@@ -3,26 +3,26 @@
 --
 
 -- Grammar for Wordy:
---    
+--
 --    word       ::= (any string)
 --    letter     ::= (any char)
 --    num        ::= (any integer)
 --    bool       ::= `true`  |  `false`
 --    prog       ::= cmd*
 --    cmd        ::= word
---                 |  bool                            
---                 |  `count`                
---                 |  `reverse`                    
---                 |  `inWord`                         
---                 |  `if` prog `else` prog `end`      
---                                                      
+--                 |  bool
+--                 |  `count`
+--                 |  `reverse`
+--                 |  `inWord`
+--                 |  `if` prog `else` prog `end`
+--
 
 
 -- Program Examples
 
 -- 1. Reverse a sentence
--- 2. Insert into a sentence 
--- 3. Add/Remove from sentence 
+-- 2. Insert into a sentence
+-- 3. Add/Remove from sentence
 -- 4. Create a valid sentence out of words
 -- 5. Check if two words are the same
 
@@ -30,18 +30,23 @@
 
 -- 1. Encode the above grammar as a set of Haskell data types
 
-
-
-type Word = String
 type Sentence = [Word]
 
 type Prog = [Cmd]
 
+data Word = Verb String
+          | Adj String
+          | Noun String
+          | Adverb String
+          | Pronoun Sting
+          | Prepisition String
+          | Conjunction String
+          | Interjection String
+          | Determiner String
+  deriving (Eq,Show)
 
-data Cmd = PushN Int
-         | PushB Bool
-         | Count word
+data Cmd = Count Word
          | Reverse Sentence
-         | Contains word Char
+         | Contains Word Char
          | IfElse Prog Prog
   deriving (Eq,Show)
